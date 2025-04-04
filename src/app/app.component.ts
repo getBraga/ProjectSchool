18c6271697595b89fc81b5e85fec9180bf0350c2
Ysmael Braga <ysmaelbraga01@gmail.com>
2025-04-04 13:47:03 -0300

    instalando e configurando o angular material

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,44 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { BreakpointObserver } from '@angular/cdk/layout';
+import { from, fromEvent, map, pluck } from 'rxjs';
+export const SCROLL_CONTENT = 'mat-sidenav-content';
+export const TEXT_LIMIT = 50;
+export const SHADOW_LIMIT = 100;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
   standalone: false,
-  styles: []
+  styles: [],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ProjectSchool';
+  public isSmallScreen = false;
+  public popText = false;
+  public applyShadow = false;
+  // @ViewChild(MatSidenav) sidenav!: MatSidenav;
+  constructor(private breakpoint: BreakpointObserver) {}
+  ngOnInit(): void {
+    const [content] = document.getElementsByClassName(SCROLL_CONTENT);
+
+    fromEvent(content, 'scroll')
+      .pipe(map(() => content.scrollTop))
+      .subscribe({
+        next: (v: number) => this.determineHeader(v),
+      });
+  }
+
+  determineHeader(scrollTop: number) {
+    this.popText = scrollTop >= TEXT_LIMIT;
+    this.applyShadow = scrollTop >= SHADOW_LIMIT;
+  }
+  ngAfterContentInit(): void {
+    this.breakpoint
+      .observe(['(max-width: 800px)'])
+      .pipe(map((x) => x.matches))
+      .subscribe((res: boolean) => (this.isSmallScreen = res));
+  }
+  get sideNavMode() {
+    return this.isSmallScreen ? 'over' : 'side';
+  }
 }
---
+++ src/shared/materials/material.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+
+import { MatListModule } from '@angular/material/list';
+import { AppComponent } from '../../app/app.component';
+@NgModule({
+  exports: [
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatListModule,
+  ],
+  imports: [],
+
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class MaterialModule {}
